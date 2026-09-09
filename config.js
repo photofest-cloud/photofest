42f6cad0007d160b89bd09bea60646cf5c421274
photofest-cloud <chimelyfotografia@gmail.com>
2026-09-09 02:59:16 -0600

    Connect PHOTO FEST frontend to Supabase public config

--- config.js
+++ config.js
@@ -1,1 +1,3 @@
+window.PHOTOFEST_SUPABASE_URL = 'https://aetztyeflblracfhallo.supabase.co';
+window.PHOTOFEST_SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_1yO3DY7cIHJT_Xxjw8X14A_VJoyPhXH';
 window.PHOTOFEST_API_URL = window.PHOTOFEST_API_URL || '';
